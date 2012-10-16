d78e53fd89f0de89723065cc70a23a5b1a735377
Jon Goodey <goodeyjon@hotmail.com>
2012-10-16 10:08:40 +0100

    php_files for location

--- location1.php
+++ location1.php
@@ -1,3 +1,22 @@
 <?php
-    echo $_POST["time"];
+$con = mysql_connect("localhost","root","");
+if (!$con)
+    {
+        die('could not connect: ' . mysql_error());
+    }
+mysql_select_db("booking_form", $con);
+$locator = $_POST["location"];
+
+$sql = mysql_query("SELECT * FROM `activities` WHERE `Location` = \"$locator\"");
+
+
+/*$result2 = mysql_query("SELECT `Location` FROM `activities` WHERE `id` = 1");*/
+
+while($row = mysql_fetch_array($sql))
+    { 
+        echo $row['Activity'] . " " . $row['Centre']. " " . $row['Location'];
+        echo "<br />";
+        
+    }
+mysql_close($con);
 ?>
